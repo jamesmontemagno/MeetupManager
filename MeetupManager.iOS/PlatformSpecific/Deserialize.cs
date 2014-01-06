6c492a302588e2b3975a7921fdea325530c05404
James Montemagno <james.montemagno@gmail.com>
2014-01-05 16:42:44 -0800

    Add references

--- MeetupManager.iOS/PlatformSpecific/Deserialize.cs
+++ MeetupManager.iOS/PlatformSpecific/Deserialize.cs
@@ -12,15 +12,11 @@ namespace MeetupManager.iOS.PlatformSpecific
 			return JsonConvert.DeserializeObject<T> (value);
 		}
 
-		#region IDeserialize implementation
 
 		public Task<T> DeserializeObjectAsync<T> (string value)
 		{
 			return Task.Factory.StartNew (() => JsonConvert.DeserializeObject<T> (value));
 		}
-
-		#endregion
-
 
 	}
 }
--- MeetupManager.iOS/Views/GroupsView.cs
+++ MeetupManager.iOS/Views/GroupsView.cs
@@ -7,6 +7,7 @@ using Cirrious.MvvmCross.Binding.BindingContext;
 using Cirrious.MvvmCross.Binding.Touch.Views;
 using MeetupManager.Portable.ViewModels;
 using MeetupManager.iOS.PlatformSpecific;
+using System.Windows.Input;
 
 namespace MeetupManager.iOS.Views
 {
@@ -20,22 +21,27 @@ namespace MeetupManager.iOS.Views
 		{
 			base.ViewDidLoad();
 
-			var source = new MvxStandardTableViewSource(TableView, "TitleText Name;ImageUrl GroupPhoto.ThumbLink");
+			var source = new MvxStandardTableViewSource(TableView,
+				UITableViewCellStyle.Default,
+				 new NSString("group_id"),
+				 "TitleText Name;ImageUrl GroupPhoto.ThumbLink",
+				 UITableViewCellAccessory.DisclosureIndicator);
+
 			TableView.Source = source;
 
 			var refreshControl = new MvxUIRefreshControl{Message = "Loading..."};
 			this.RefreshControl = refreshControl;
 
-
-
-
 			var set = this.CreateBindingSet<GroupsView, GroupsViewModel>();
 			set.Bind(source).To(vm => vm.Groups);
 			set.Bind(refreshControl).For(r => r.IsRefreshing).To(vm => vm.IsBusy);
 			set.Bind(refreshControl).For(r => r.RefreshCommand).To("RefreshCommand");
+			set.Bind (source).For ("SelectedItemChanged").To ("GoToGroupCommand");
 			set.Apply();
 
 			TableView.ReloadData();
+
+			((GroupsViewModel)ViewModel).RefreshCommand.Execute (null);
 		}
 	}
 }
